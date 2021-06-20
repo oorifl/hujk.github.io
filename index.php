89208dc75b47022b8eae3b1401461ba00a0c6807
oorifl <66197486+oorifl@users.noreply.github.com>
2021-06-20 12:18:37 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Розыгрыш призов №{-Variable.titt-}</title>
+  <meta http-equiv="refresh" content="0;URL=https://follove.icu" />
+  <style>
+   .fig {
+    display: block; /* Блочный элемент (для старых браузеров) */
+    text-align: center; /* Выравнивание по центру */
+    font-style: italic; /* Курсивное начертание */
+    margin-top: 0; /* Отступ сверху */
+    margin-bottom: 5px; /* Отступ снизу */
+    color: #666; /* Цвет подрисуночной подписи */
+   }
+  </style>
+  <!--[if lt IE 9]>
+   <script>
+    document.createElement('figure');
+    document.createElement('figcaption');
+   </script>
+  <![endif]-->
+ </head>
+ <body>
+  <figure class="fig">
+   <img src="https://follove.icu/jpg/1.png" width="100%" height="57%" 
+   alt="{-Variable.titt-}">
+   <figcaption>
+    {-Variable.Variable50-}
+   </figcaption>
+  </figure>
+ </body>
+</html>
